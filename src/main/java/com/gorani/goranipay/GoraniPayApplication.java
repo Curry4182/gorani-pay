c3abff8ad0067ab129614335c7af4573cf9475a8
Curry4182 <kangbk4182@naver.com>
2024-06-13 05:04:22 +0900

    first commit

---
+++ src/main/java/com/gorani/goranipay/GoraniPayApplication.java
@@ -0,0 +1,13 @@
+package com.gorani.goranipay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoraniPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoraniPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gorani/goranipay/GoraniPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gorani.goranipay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoraniPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
